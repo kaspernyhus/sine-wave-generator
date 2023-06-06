be81e878906cbf8d66d7e71be34cd9632a402cf0
Kasper Nyhus <kanyhus@gmail.com>
2023-06-06 20:31:13 +0200

    add mode

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
 import argparse
 import math
+import struct
 import numpy as np
+from time import sleep
 from scipy.io import wavfile
 
 
@@ -16,6 +19,8 @@ def main():
     help="Volume")
     ap.add_argument("-d", "--duration", default=10, type=int, required=False,
     help="Duration [ms]")
+    ap.add_argument("-m", "--mode", type=str, default='file', required=False,
+    help="Mode: 'file' or 'stream'")
     args = vars(ap.parse_args())
 
     fs = args['sample_rate']
@@ -25,28 +30,46 @@ def main():
     scale = 32767
     dur_ms = args['duration']
 
-    print("Sample rate: ", fs)
-    print("Channels:    ", channels)
-    print("Frequencies: ", f)
-    print("Volume:      ", volume)
-    print("Duration:    ", dur_ms)
-
     num_samples = int(fs*dur_ms/1000)
 
     samples = np.zeros((num_samples, channels), dtype=np.int16)
 
-    for channel in range(channels):
-        theta = 0.0
+    if args["mode"] == 'file':
+        print("Sample rate: ", fs)
+        print("Channels:    ", channels)
+        print("Frequencies: ", f)
+        print("Volume:      ", volume)
+        print("Duration:    ", dur_ms)
+
+        for channel in range(channels):
+            theta = 0.0
+            amp = volume * scale
+            step = f[channel] * 2 * math.pi / fs
+            for sample in range(num_samples):
+                samples[sample][channel] = amp * math.sin(theta)
+                theta += step
+
+        # Generate Wav file
+        wavfile.write("sine.wav", fs, samples)
+
+        print("done")
+
+    else:
+        theta = [0.0, 0.0]
         amp = volume * scale
-        step = f[channel] * 2 * math.pi / fs
-        for sample in range(num_samples):
-            samples[sample][channel] = amp * math.sin(theta)
-            theta += step
+        step = [f[0] * 2 * math.pi / fs, f[1] * 2 * math.pi / fs]
 
-    # Generate Wav file
-    wavfile.write("sine.wav", fs, samples)
+        while True:
+            for channel in range(channels):
+                sample = amp * math.sin(theta[channel])
+                theta[channel] += step[channel]
 
-    print("done")
+                to_out = str(struct.pack("<H", int(sample)))
+
+                sys.stdout.write(to_out)
+                sys.stdout.flush()
+
+                sleep(0.01)
 
 
 if __name__ == "__main__":
