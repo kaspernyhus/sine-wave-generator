33d49a8529817d661b4f57b60d7212400ae0e6de
Kasper Nyhus <kanyhus@gmail.com>
2024-10-29 20:58:10 +0100

    generate sine wave stream

---
+++ generate_sine_stream.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import signal
+import argparse
+from sine_wave_generator import SineWaveGenerator
+
+
+def main():
+    def signal_handler(sig, frame):
+        print("\nCtrl+C pressed. Shutting down...")
+        generator.stop()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    ap = argparse.ArgumentParser(description="Sine generator")
+    ap.add_argument(
+        "-r",
+        "--sample_rate",
+        default=48000,
+        required=False,
+        type=int,
+        help="Sample Rate",
+    )
+    ap.add_argument(
+        "-f",
+        "--frequency",
+        default=440,
+        required=False,
+        type=int,
+        help="Sine frequency",
+    )
+    ap.add_argument(
+        "-v",
+        "--volume",
+        default=0.8,
+        type=float,
+        required=False,
+        help="Volume"
+    )
+    args = vars(ap.parse_args())
+
+    fs = args["sample_rate"]
+    freq = args["frequency"]
+    volume = args["volume"]
+
+    generator = SineWaveGenerator(sample_rate=fs, frequency=freq, chunk_size=1024)
+    generator.start()
+
+    while not generator.exit_event.is_set():
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
